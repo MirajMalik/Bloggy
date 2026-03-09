72704998fecf5439c3a2bc9edf53018c8a12c519
MirajMalik <mirajmalik333@gmail.com>
2026-03-09 17:31:47 +0600

    created post and user schema, login and logout page with jwt token

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/bloggy");
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    posts: [{                                                      // array of id's
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "post"
+    }],
+})
+
+const userModel = mongoose.model("user",userSchema);
+
+
+module.exports = userModel;
